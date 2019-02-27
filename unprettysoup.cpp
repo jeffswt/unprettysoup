f141b90513382f39fcd9eeb40b37d8060275f8dd
jeffswt <jeffswt@users.noreply.github.com>
2019-02-27 23:27:48 +0800

    Add string initializer functions.

--- unprettysoup.cpp
+++ unprettysoup.cpp
@@ -2,5 +2,39 @@
 #include "unprettysoup.h"
 
 
-class us3::String
-{};
+us3::String::String(void)
+{
+    this->contents = std::string();
+    return ;
+}
+
+us3::String::String(std::string str)
+{
+    this->contents = str;
+    return ;
+}
+
+us3::String::String(char* str)
+{
+    this->contents = "";
+    this->contents.assign(str, strlen(str));
+    return ;
+}
+
+us3::String::String(const char* str)
+{
+    this->contents = std::string(str);
+    return ;
+}
+
+std::istream& us3::operator >> (std::istream& stream, String& str)
+{
+    stream >> str.contents;
+    return stream;
+}
+
+std::ostream& us3::operator << (std::ostream& stream, const us3::String& str)
+{
+    stream << str.contents;
+    return stream;
+}
--- unprettysoup.h
+++ unprettysoup.h
@@ -2,13 +2,32 @@
 #ifndef UNPRETTYSOUP_H
 #define UNPRETTYSOUP_H
 
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
 #include <vector>
 
 
 namespace us3
 {
     class String
-    {};
+    {
+    private:
+    protected:
+        std::string contents;
+    public:
+        String(void);
+        String(std::string);
+        String(char*);
+        String(const char*);
+        friend std::istream& operator >> (std::istream&, String&);
+        friend std::ostream& operator << (std::ostream&, const String&);
+    };
+
+    std::istream& operator >> (std::istream&, String&);
+    std::ostream& operator << (std::ostream&, const String&);
 } // us3
 
 #endif
