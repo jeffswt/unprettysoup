ef7326c1ca08ca904eff20ca078fb79af70a672c
jeffswt <jeffswt@users.noreply.github.com>
2019-03-03 14:03:40 +0800

    Add Unicode-safe .isdecimal, .isdigit, .isnumeric methods.

--- unprettysoup.h
+++ unprettysoup.h
@@ -47,8 +47,10 @@ namespace us3
         // Determination methods
         bool isalnum(void) const;
         bool isalpha(void) const;
+        bool isdecimal(void) const;
         bool isdigit(void) const;
         bool islower(void) const;
+        bool isnumeric(void) const;
         bool isspace(void) const;
         bool isupper(void) const;
         // Input and output
@@ -110,8 +112,10 @@ namespace us3
         int find_last_not_of(const std::set<Char>&, int) const;
         bool isalnum(void) const;
         bool isalpha(void) const;
+        bool isdecimal(void) const;
         bool isdigit(void) const;
         bool islower(void) const;
+        bool isnumeric(void) const;
         bool isspace(void) const;
         bool isupper(void) const;
         String join(const std::vector<String>&) const;
